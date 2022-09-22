a9f6848bcc2988969ed872b862fabb41e8dde1ca
ajp84 <prater.anthony.j@gmail.com>
2022-09-21 18:55:09 -0700

    styled mobile menu

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ document
   .addEventListener("click", function (event) {
     event.preventDefault();
   });
+
+function hamburger() {
+  let hamburgerNav = document.querySelector(".hamburger-menu-nav-wrapper");
+  let headerEl = document.querySelector(".homepage-hero-intro");
+
+  if (hamburgerNav.style.display === "grid") {
+    hamburgerNav.style.display = "none";
+    headerEl.style.backgroundColor = "unset";
+  } else {
+    hamburgerNav.style.display = "grid";
+    headerEl.style.backgroundColor = "#14131c";
+    // headerEl.style.zIndex = "4";
+  }
+}
